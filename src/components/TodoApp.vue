faef3b3c86c8f6ac3736b7c6581373489eb3a679
Ashley Dennis <adenn1852@gmail.com>
2022-07-16 17:58:30 -0400

    first push

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id" data-test="todo">
+        {{ todo.text}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoApp',
+  data(){
+    return {
+        todos: [
+            {
+                id: 1,
+                text: "Learn Vue Testing",
+                completed: false
+            }
+        ],
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
