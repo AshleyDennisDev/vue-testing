71cf33933e35a431322ff759477f3c6a3b05ecfb
Ashley Dennis <adenn1852@gmail.com>
2022-07-24 20:44:05 -0400

    used async await in tests

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <div v-for="todo in todos" :key="todo.id" data-test="todo">
+    <div 
+    v-for="todo in todos" 
+    :key="todo.id" 
+    data-test="todo"
+    :class="[todo.completed ? 'completed' : '']"
+    >
         {{ todo.text}}
+        <input
+        type="checkbox"
+        v-model="todo.completed"
+        data-test="todo-checkbox"
+        />
     </div>
+
+    <form data-test="form" @submit.prevent="createTodo">
+        <input data-test="new-todo" v-model="newTodo"/>
+    </form>
   </div>
 </template>
 
@@ -11,6 +25,7 @@ export default {
   name: 'TodoApp',
   data(){
     return {
+        newTodo:"",
         todos: [
             {
                 id: 1,
@@ -19,11 +34,22 @@ export default {
             }
         ],
     }
+  },
+  methods: {
+    createTodo(){
+        this.todos.push({
+            id: 2,
+            text: this.newTodo,
+            completed: false
+        })
+    }
   }
+  
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.completed {
+    text-decoration: line-through;
+}
 </style>
--- tests/unit/TodoApp.spec.js
+++ tests/unit/TodoApp.spec.js
@@ -9,4 +9,21 @@ describe("TodoApp.vue", () => {
 
         expect(todo.text()).toBe("Learn Vue Testing")
     })
-})
+    it("should add new todo", async() => { 
+        const wrapper = mount(TodoApp);
+
+        expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(1);
+        
+        await wrapper.get('[data-test="new-todo"]').setValue('New Todo')
+        await wrapper.get('[data-test="form"]').trigger('submit')
+
+        expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2);
+    });
+    it("should be able to complete todo", async() => { 
+        const wrapper = mount(TodoApp);
+        
+        await wrapper.get('[data-test="todo-checkbox"]').setValue(true);
+
+        expect(wrapper.get('[data-test="todo"]').classes()).toContain("completed");
+    });
+});
